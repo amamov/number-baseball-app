ee8365056014cf290d022aad7a913394eb47eb0e
Yoon sang seok <63184031+amamov@users.noreply.github.com>
2020-10-09 00:52:50 +0900

    start

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { AppLoading } from "expo";
+import { StatusBar } from "expo-status-bar";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { Image, Text } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const cacheImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") return Image.prefetch(image);
+    else return Asset.fromModule(image).downloadAsync();
+  });
+const cacheFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+function App() {
+  const [isReady, setIsReady] = useState(false);
+  const [fontLoaded] = Font.useFonts({
+    NanumPenScript: require("./assets/NanumPenScript-Regular.ttf"),
+  });
+
+  const loadAssets = () => {
+    const images = cacheImages([
+      "https://images.unsplash.com/photo-1503104834685-7205e8607eb9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+      require("./assets/splash.png"),
+    ]);
+
+    const fonts = cacheFonts([Ionicons.font]);
+
+    return Promise.all([...images, ...fonts]);
+  };
+
+  const onFinish = () => setIsReady(true);
+
+  return isReady && fontLoaded ? (
+    <Text style={{ fontFamily: "NanumPenScript" }}>hello fucking world</Text>
+  ) : (
+    <AppLoading
+      startAsync={loadAssets}
+      onFinish={onFinish}
+      onError={(error) => console.log(error.message)}
+    />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
